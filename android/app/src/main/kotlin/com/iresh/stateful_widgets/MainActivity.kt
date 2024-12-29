b764731a3443a92409a207376e5245d3733a2e13
Iresh Eranga <iresheranga2020al@gmail.com>
2024-12-29 20:04:19 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/iresh/stateful_widgets/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.iresh.stateful_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Stateful Widgets Flutter",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: CTStatefulClass(),
+      ),
+    );
+  }
+}
+
+class CTStatefulClass extends StatefulWidget {
+  const CTStatefulClass({super.key});
+
+  @override
+  State<CTStatefulClass> createState() => _CTStatefulClassState();
+}
+
+class _CTStatefulClassState extends State<CTStatefulClass> {
+  bool liked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          title: Text("Nike Shoes"),
+          trailing: IconButton(
+            onPressed: () {
+              setState(
+                () {
+                  liked = !liked;
+                },
+              );
+            },
+            icon: liked ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
+          ),
+        ),
+      ],
+    );
+  }
+}
