6a774a94e05e13445605eac183da38553889017e
Iresh Eranga <iresheranga2020al@gmail.com>
2024-12-31 09:49:19 +0530

    check box

--- lib/details.dart
+++ lib/details.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class DetailsForm extends StatelessWidget {
-  const DetailsForm({super.key});
+  final String productName;
+  final String productDescription;
+
+  const DetailsForm({
+    super.key,
+    required this.productName,
+    required this.productDescription,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +20,42 @@ class DetailsForm extends StatelessWidget {
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back,color: Colors.white,size: 20.0,),
+          icon: Icon(Icons.arrow_back, color: Colors.white, size: 20.0),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
       ),
-      body: Center(
-        child: Text("Details Screen"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Card(
+              elevation: 4,
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.label, color: Colors.blue),
+                title: Text(
+                  "Product Name",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(productName),
+              ),
+            ),
+            Card(
+              elevation: 4,
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: ListTile(
+                leading: Icon(Icons.description, color: Colors.blue),
+                title: Text(
+                  "Product Description",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(productDescription),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/form.dart
+++ lib/form.dart
@@ -9,6 +9,17 @@ class MyForm extends StatefulWidget {
 }
 
 class _MyFormState extends State<MyForm> {
+  final _productController = TextEditingController();
+  final _productDesController = TextEditingController();
+  bool? _checkBox, _listTileCheckBox = false;
+
+  @override
+  void dispose() {
+    _productController.dispose();
+    _productDesController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,20 +31,82 @@ class _MyFormState extends State<MyForm> {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: OutlinedButton(
-          style: OutlinedButton.styleFrom(
-            minimumSize: const Size(200, 50),
-          ),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return DetailsForm(); // Navigate to DetailsForm
-            }));
-          },
-          child: Text(
-            "Submit Form".toUpperCase(),
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            TextFormField(
+              controller: _productController,
+              decoration: InputDecoration(
+                labelText: 'Product Name',
+                prefixIcon: Icon(Icons.label),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              controller: _productDesController,
+              decoration: InputDecoration(
+                labelText: 'Product Description',
+                prefixIcon: Icon(Icons.description),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(minimumSize: const Size(200, 50)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return DetailsForm(
+                        productName: _productController.text,
+                        productDescription: _productDesController.text,
+                      );
+                    },
+                  ),
+                );
+              },
+              child: Text(
+                "Submit Form".toUpperCase(),
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.deepPurple),
+              ),
+            ),
+
+            //two types of check boxes
+
+            //1.checkBox
+            Checkbox(
+                tristate: true,
+                checkColor: Colors.white,
+                activeColor: Colors.purple,
+                value: _checkBox,
+                onChanged: (val) {
+                  setState(() {
+                    _checkBox = val;
+                  });
+                }),
+
+            //2. check box list tile
+            CheckboxListTile(
+              checkColor: Colors.white,
+              activeColor: Colors.purple,
+              title: Text("Top Product"),
+              tristate: true,
+              value: _listTileCheckBox,
+              onChanged: (val) {
+                setState(
+                  () {
+                    _listTileCheckBox = val;
+                  },
+                );
+              },
+              //leading check box
+              controlAffinity: ListTileControlAffinity.leading,
+            ),
+          ],
         ),
       ),
     );
